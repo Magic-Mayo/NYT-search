e0871cd4196082b4aa48572cfb0c59ce4e0e56e3
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-06-18 21:37:47 -0700

    what we finished in class

--- nyt.js
+++ nyt.js
@@ -1,20 +1,26 @@
 //initial variables for pulling from the api
-const queryURL = "https://api.nytimes.com/svc/topstories/v2/science.json?api-key=d2cPepavANvAE81Lc28tk5FBv9eaRrbl";
+$("<button>").appendTo("body").text("search")
+const queryURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=d2cPepavANvAE81Lc28tk5FBv9eaRrbl";
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-  .then(function(response){
-      
-      for (let i = 0; i < records; i++){
-          var div = $("<div>")
-          div.append(response.title);
+  url: queryURL,
+  method: "GET"
+})
+  .then(function (response) {
+    // var records=$(".js-records")
 
-        }
-      
-      
+    for (let i = 0; i < 5; i++) {
+      var div = $("<div>")
+      div.append(response.results.title).addClass("css-results");
 
-  console.log(response.title);
-  console.log(response.url);
+
+    }
+    $("button").on("click", function () {
+      $(".js-search").val().trim();
+      
+    });
+
+
+    console.log(response);
+    console.log(response.results);
   });
